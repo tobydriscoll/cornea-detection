b5c2f518e0b8c781435976c5c50a43efb6eb10d2
Toby Driscoll <driscoll@udel.edu>
2020-09-30 10:06:35 -0400

    new stuff

--- src/CorneaDetection.jl
+++ src/CorneaDetection.jl
@@ -5,10 +5,11 @@ using ImageIO,VideoIO
 using Images,ImageFiltering,ImageTransformations,Interpolations
 using Optim,Statistics,ProgressMeter
 
-export detect,detectfolder,detectmovie
+export findpurkinje,detect,detectfolder,detectmovie,makemovie
 
 include("optimize.jl")
 include("process.jl")
 include("show.jl")
+include("summarize.jl")
 
 end # module
