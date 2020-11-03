8139eca0b55aed760e4784fdb7ccd4cc49e75bea
Toby Driscoll <driscoll@udel.edu>
2020-11-03 13:04:00 -0500

    add summary to resize

--- src/summarize.jl
+++ src/summarize.jl
@@ -2,19 +2,57 @@ resize(root::String,subj::Number,vis::Number,tri::Number,args...) = resize(root,
 function resize(rootin::String,rootout::String,subj,vis,tri,sz=(2824÷2,4240÷2))
 	T = Trial(rootin,subj,vis,tri)
 	outdir = joinpath(rootout,dirname(T))
-	summary = (fname = filenames(T), size = [], purkrow = Float64[], purkcol = Float64[], freqR = Vector{Float64}[], freqG = Vector{Float64}[], freqB = Vector{Float64}[]  )
-	@showprogress for fname in filenames(T,join=true)
+	summary = (fname = filenames(T), 
+	size = [], 
+	avgG = Float32[], 
+	purkrow = Float32[], 
+	purkcol = Float32[], 
+	freqR = [], 
+	freqG = [], 
+	freqB = []  )
+	@showprogress for (idx,fname) in enumerate(filenames(T,join=true))
 		X = load(fname)
 		X = imresize(X,sz)
 		push!(summary.size,sz)
 		pngname = splitext(basename(fname))[1]*".png"
 		save(joinpath(outdir,pngname),X)
+		summary.fname[idx] = pngname
+		
+		purk = findpurkinje(X,0.5)
+		if length(purk) > 40
+			push!(summary.purkrow, median(i[1] for i in purk))
+			push!(summary.purkcol, median(i[2] for i in purk))
+		else
+			push!(summary.purkrow, NaN)
+			push!(summary.purkcol, NaN)
+		end
+		edges = collect(LinRange(0,1,33))
+		edges[1] = -eps()
+		
+		push!(summary.avgG,mean(green.(X)))
+		
+		w = fit(Histogram,vec(red.(X)),edges,closed=:right).weights
+		push!(summary.freqR,w/prod(sz))
+		w = fit(Histogram,vec(green.(X)),edges,closed=:right).weights
+		push!(summary.freqG,w/prod(sz))
+		w = fit(Histogram,vec(blue.(X)),edges,closed=:right).weights
+		push!(summary.freqB,w/prod(sz))
 	end
+	CSV.write(joinpath(outdir,"summary.csv"),summary)
+	tmp = joinpath(tempname(),"summary.jld2")
+	save(tmp,shortname(T),summary)
+	cp(tmp,joinpath(outdir,"summary.jld2"),force=false)
+	return summary
 end
 
-function summarize(root,subj,vis,tri)
-	trial = Trial(root,subj,vis,tri)
-	summary = (fname = filenames(trial), size = [], avgG = Float32[], purkrow = Float32[], purkcol = Float32[], freqR = [], freqG = [], freqB = []  )
+summarize(root,subj,vis,tri) = summarize(Trial(root,subj,vis,tri))
+function summarize(trial::Trial)
+	summary = (fname = filenames(trial), 
+	size = [], 
+	avgG = Float32[], 
+	purkrow = Float32[], 
+	purkcol = Float32[], 
+	freqR = [], freqG = [], freqB = []  )
 	@showprogress for fname in filenames(trial,join=true)
 		X = load(fname)
 		sz = size(X)
