e452057ae47e517da930d2c3b9d7f4f7dac65fe3
Toby Driscoll <driscoll@udel.edu>
2020-11-12 15:49:22 -0500

    access by folder not s/v/t

--- src/summarize.jl
+++ src/summarize.jl
@@ -1,99 +1,139 @@
-resize(root::String,subj::Number,vis::Number,tri::Number,args...) = resize(root,".",subj,vis,tri,args...)
-function resize(rootin::String,rootout::String,subj,vis,tri,sz=(2824÷2,4240÷2))
-	T = Trial(rootin,subj,vis,tri)
-	outdir = joinpath(rootout,dirname(T))
-	summary = (fname = filenames(T), 
-	size = [], 
-	avgG = Float32[], 
-	purkrow = Float32[], 
-	purkcol = Float32[], 
-	freqR = [], 
-	freqG = [], 
-	freqB = []  )
-	@showprogress for (idx,fname) in enumerate(filenames(T,join=true))
+function resize(folder::AbstractVector{String},dest,sz)
+	N = length(folder)
+	summary = ( 
+		size = fill([0,0],N), 
+		avgG = zeros(N), 
+		purkrow = fill(NaN,N), 
+		purkcol = fill(NaN,N), 
+		freqR = fill([NaN],N), 
+		freqG = fill([NaN],N),
+		freqB = fill([NaN],N)
+	)
+	@showprogress for (idx,fname) in enumerate(folder)
 		X = load(fname)
 		X = imresize(X,sz)
-		push!(summary.size,sz)
 		pngname = splitext(basename(fname))[1]*".png"
-		save(joinpath(outdir,pngname),X)
-		summary.fname[idx] = pngname
-		
-		purk = findpurkinje(X,0.5)
-		if length(purk) > 40
-			push!(summary.purkrow, median(i[1] for i in purk))
-			push!(summary.purkcol, median(i[2] for i in purk))
-		else
-			push!(summary.purkrow, NaN)
-			push!(summary.purkcol, NaN)
-		end
-		edges = collect(LinRange(0,1,33))
-		edges[1] = -eps()
-		
-		push!(summary.avgG,mean(green.(X)))
-		
-		w = fit(Histogram,vec(red.(X)),edges,closed=:right).weights
-		push!(summary.freqR,w/prod(sz))
-		w = fit(Histogram,vec(green.(X)),edges,closed=:right).weights
-		push!(summary.freqG,w/prod(sz))
-		w = fit(Histogram,vec(blue.(X)),edges,closed=:right).weights
-		push!(summary.freqB,w/prod(sz))
+		save(joinpath(dest,pngname),X)
+		update_summary!(summary,idx,X)
+	
 	end
-	CSV.write(joinpath(outdir,"summary.csv"),summary)
-	tmp = joinpath(tempname(),"summary.jld2")
-	save(tmp,shortname(T),summary)
-	cp(tmp,joinpath(outdir,"summary.jld2"),force=false)
 	return summary
 end
 
-summarize(root,subj,vis,tri) = summarize(Trial(root,subj,vis,tri))
-function summarize(trial::Trial)
-	summary = (fname = filenames(trial), 
-	size = [], 
-	avgG = Float32[], 
-	purkrow = Float32[], 
-	purkcol = Float32[], 
-	freqR = [], freqG = [], freqB = []  )
-	@showprogress for fname in filenames(trial,join=true)
-		X = load(fname)
-		sz = size(X)
-		push!(summary.size,[sz...])
-		
-		purk = findpurkinje(X,0.5)
-		if length(purk) > 40
-			push!(summary.purkrow, median(i[1] for i in purk))
-			push!(summary.purkcol, median(i[2] for i in purk))
-		else
-			push!(summary.purkrow, NaN)
-			push!(summary.purkcol, NaN)
-		end
-		edges = collect(LinRange(0,1,33))
-		edges[1] = -eps()
-		
-		push!(summary.avgG,mean(green.(X)))
-		
-		w = fit(Histogram,vec(red.(X)),edges,closed=:right).weights
-		push!(summary.freqR,w/prod(sz))
-		w = fit(Histogram,vec(green.(X)),edges,closed=:right).weights
-		push!(summary.freqG,w/prod(sz))
-		w = fit(Histogram,vec(blue.(X)),edges,closed=:right).weights
-		push!(summary.freqB,w/prod(sz))
+function summarize(folder::AbstractVector{String})
+	N = length(folder)
+	summary = ( 
+		size = fill([0,0],N), 
+		avgG = zeros(N), 
+		purkrow = fill(NaN,N), 
+		purkcol = fill(NaN,N), 
+		freqR = fill([NaN],N), 
+		freqG = fill([NaN],N),
+		freqB = fill([NaN],N)
+	)
+	@showprogress for (idx,fname) in enumerate(folder)
+		update_summary!(summary,idx,load(fname))
 	end
-	CSV.write(joinpath(fullname(trial),"summary.csv"),summary)
-	tmp = joinpath(tempname(),"summary.jld2")
-	save(tmp,shortname(trial),summary)
-	cp(tmp,joinpath(fullname(trial),"summary.jld2"),force=false)
 	return summary
 end
 
-function cleanup(T::Trial)
+function update_summary!(summary,idx,X)
+	sz = size(X)
+	summary.size[idx] = [sz...]
+	
+	purk = findpurkinje(X,0.65)
+	if length(purk) > 40
+		summary.purkrow[idx] = median(i[1] for i in purk)
+		summary.purkcol[idx] = median(i[2] for i in purk)
+	end
+	edges = collect(LinRange(0,1,33))
+	edges[1] = -eps()
+	
+	summary.avgG[idx] = mean(green.(X))
+	
+	w = fit(Histogram,vec(red.(X)),edges,closed=:right).weights
+	summary.freqR[idx] = w/prod(sz)
+	w = fit(Histogram,vec(green.(X)),edges,closed=:right).weights
+	summary.freqG[idx] = w/prod(sz)
+	w = fit(Histogram,vec(blue.(X)),edges,closed=:right).weights
+	summary.freqB[idx] = w/prod(sz)
 
 end
+
+
+function intensity(T::Trial)
+	s = summary(T)
+	bins = (0.5:31.5)/33
+	intensity = Float64[]
+	for (i,distr) in enumerate(s.freqG)
+		# data might be in string form if it was loaded from CSV
+		if !(eltype(distr) <: Number)
+			distr = parse.(Float64,split(replace(distr[2:end-1],","=>" ")))
+		end
+		push!(intensity,sum(bins[j]*distr[j] for j in 1:31))
+	end
+	intensity
+end
+
+function finddark(T::Trial)
+	res = outerjoin(summary(T),results(T),on=:fname,makeunique=true)
+	ip,jp = res.purkrow,res.purkcol
+	nopurk = isnan.(ip)
+	q = intensity(T)
+	dark = @. ( q < 0.15 ) | ( nopurk & (q < 0.25) )
+	ct = 3
+	while ct < count(dark)
+		ct = count(dark)
+		bright = q[.!dark]
+		μ = median(bright)
+#		if count(dark) > 3
+		σ = 0.15*(μ - median(q[dark]))
+#		end
+		dark = @. dark | (q < μ - 2σ)
+	end
+
+	# N = length(dark)
+	# Δ = diff(intensity)
+	# # for n in 1:N
+	# # 	if n < N
+	# # 		dark[n] |= (dark[n+1] & (intensity[n] < μ-σ))			
+	# # 	end
+	# # 	if n > 1
+	# # 		dark[n] |= (dark[n-1] & (intensity[n] < μ-σ))
+	# # 	end
+	# # end 
+	# dark[1] |= dark[2]
+	# dark[N] |= dark[N-1]
+	# # for n in 2:N-1
+	# # 	s = intensity[n+1] - intensity[n-1]
+	# # 	dark[n] |= ( dark[n+1] & (s < -2σ) ) || ( dark[n-1] & (s > 2σ) )
+	# end
+	return dark
+
+end
+
+function goodframes(T::Trial)
+	isdark = CorneaDetection.finddark(T);
+
+	N = length(isdark)
+	w = [ sum(isdark[i:i+2]) for i in 1:N-2 ]
+	#@. isdark[2:N-1] |= (isdark[1:N-2] & isdark[3:N])  # peer pressure
+	#fd = findall(isdark)
+	fd = findall(w.>1)
+	idx = findfirst(diff(fd).>2)
+	!isnothing(idx) && (isdark[1:fd[idx]] .= true)
+	!isnothing(idx) && (isdark[fd[idx+1]:end] .= true)
+
+	return .!isdark
+end
+
 
 backfill(V::Visit) = foreach(backfill,trials(V))
 function backfill(T::Trial)
 	s = summary(T)
 	resdir = "/Users/driscoll/Dropbox/research/tearfilm/cornea/version4/data"
 	r = rightjoin(results(T,resdir),s,on=:fname)
+	sort!(r,:fname)
 	folder = filenames(T,join=true)
 	sz = size(load(folder[1]))
 	@showprogress for (idx,fname) in enumerate(folder)
