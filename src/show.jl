ba09b524d669531fc6af506a22773793239e5c33
Toby Driscoll <driscoll@udel.edu>
2020-12-10 10:31:14 -0500

    draw better circle

--- src/show.jl
+++ src/show.jl
@@ -3,7 +3,7 @@
 Modifies the image to superimpose a circle of specified center and radius. If `trange` is given, it's a 2-vector defining the range of angles to be used, measured ccw from "straight down" in the usual image visualization (i.e., vertically flipped).
 """
 function drawcircle!(img,i,j,r,trange::AbstractVector=[-π,π];usecolor=RGB(1,0,0.8))
-	t = π/2 .+ LinRange(trange[1],trange[2],size(img,2))
+	t = π/2 .+ LinRange(trange[1],trange[2],5size(img,2))
 	valid = i -> checkbounds(Bool,img,i)
 	for r in 0.99r:1.01r
 		idx = CartesianIndex.(zip(round.(Int,i.+r*sin.(t)),round.(Int,j.+r*cos.(t))))
